05f99c6fb2f1070a2d01b358e613daecc99c6cfa
kode-green <99124687+kode-green@users.noreply.github.com>
2022-02-06 17:39:52 +0000

    v

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,21 @@
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.kodeConnect = kodeConnect;
+
 var _ably = _interopRequireDefault(require("ably"));
 
 var _ablyCommonjs = require("ably/browser/static/ably-commonjs.js");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-module.exports = function kodeConnect(region, appId, apiKey) {
+function kodeConnect(region, appId, apiKey) {
   window.Ably = new _ablyCommonjs.Realtime(region);
   window.Ably.connection.on('connected', function () {
     console.log("Connected to kode");
   });
   var channel = window.Ably.channels.get("".concat(appId, "-").concat(apiKey));
   channel.publish("auth", "".concat(appId, "-connected"));
-};
+}
