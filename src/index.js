0696c1a6b2df344e0f88cedd428604f1226ec40e
kode-green <99124687+kode-green@users.noreply.github.com>
2022-02-06 17:38:34 +0000

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import Ably from 'ably';
 import { Realtime } from 'ably/browser/static/ably-commonjs.js';
 
-module.exports = function kodeConnect(region, appId, apiKey) {
+export function kodeConnect(region, appId, apiKey) {
   window.Ably = new Realtime(region);
   window.Ably.connection.on('connected', function() {
     console.log("Connected to kode");
