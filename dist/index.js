281069e6488924f449d76ef5a97a21d7338059c7
kode-green <99124687+kode-green@users.noreply.github.com>
2022-02-06 23:03:09 +0000

    v

--- dist/index.js
+++ dist/index.js
@@ -3,13 +3,53 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.kodeAuth = kodeAuth;
 exports.kodeConnect = kodeConnect;
+exports.kodeFlow = kodeFlow;
 
 var _ably = _interopRequireDefault(require("ably"));
 
 var _ablyCommonjs = require("ably/browser/static/ably-commonjs.js");
 
+var _auth0SpaJs = _interopRequireDefault(require("@auth0/auth0-spa-js"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
+
+function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
+
+function kodeAuth(_x, _x2, _x3, _x4, _x5, _x6) {
+  return _kodeAuth.apply(this, arguments);
+}
+
+function _kodeAuth() {
+  _kodeAuth = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(region, appId, domain, clientId, callBackUrl, audience) {
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            window.Ably = new _ablyCommonjs.Realtime(region);
+            (0, _auth0SpaJs["default"])({
+              domain: domain,
+              client_id: clientId,
+              redirect_uri: callBackUrl,
+              audience: audience
+            }).then(function (auth0) {
+              var channel = window.Ably.channels.get("".concat(appId, "-token"));
+              channel.publish("".concat(appId, "-token"), auth0);
+              console.log("Token", auth0);
+            });
+
+          case 2:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee);
+  }));
+  return _kodeAuth.apply(this, arguments);
+}
 
 function kodeConnect(region, appId, apiKey) {
   window.Ably = new _ablyCommonjs.Realtime(region);
@@ -17,5 +57,10 @@ function kodeConnect(region, appId, apiKey) {
     console.log("Connected to kode");
   });
   var channel = window.Ably.channels.get("".concat(appId, "-").concat(apiKey, "-auth"));
+  channel.publish("".concat(appId, "-").concat(apiKey, "-auth"), "connected_user_".concat(navigator.userAgent));
+}
+
+function kodeFlow(flowId, appId, data) {
+  var channel = window.Ably.channels.get("".concat(flowId, "-").concat(apiKey, "-flow"));
   channel.publish("".concat(appId, "-").concat(apiKey, "-auth"), "connected_user_".concat(navigator.userAgent));
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 import Ably from 'ably';
 import { Realtime } from 'ably/browser/static/ably-commonjs.js';
+import createAuth0Client from '@auth0/auth0-spa-js';
+
+export async function kodeAuth(region, appId, domain, clientId, callBackUrl, audience) {
+  window.Ably = new Realtime(region);
+  createAuth0Client({
+    domain,
+    client_id: clientId,
+    redirect_uri: callBackUrl,
+    audience: audience
+  }).then(auth0 => {
+    const channel = window.Ably.channels.get(`${appId}-token`);
+    channel.publish(`${appId}-token`, auth0);
+    console.log("Token", auth0)
+  });
+}
 
 export function kodeConnect(region, appId, apiKey) {
   window.Ably = new Realtime(region);
@@ -7,5 +22,10 @@ export function kodeConnect(region, appId, apiKey) {
     console.log("Connected to kode");
   });
   const channel = window.Ably.channels.get(`${appId}-${apiKey}-auth`);
+  channel.publish(`${appId}-${apiKey}-auth`, `connected_user_${navigator.userAgent}`);
+}
+
+export function kodeFlow(flowId, appId, data) {
+  const channel = window.Ably.channels.get(`${flowId}-${apiKey}-flow`);
   channel.publish(`${appId}-${apiKey}-auth`, `connected_user_${navigator.userAgent}`);
 }
