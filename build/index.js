0afc3296003ec1bd42a46d96a0bd3205a5ae7a17
kode-green <99124687+kode-green@users.noreply.github.com>
2022-02-06 14:27:06 +0000

    ini

---
+++ build/index.js
@@ -0,0 +1,11 @@
+import Ably from 'ably';
+import { Realtime } from 'ably/browser/static/ably-commonjs.js';
+
+export default function kodeConnect(region, appId, apiKey) {
+  window.Ably = new Realtime(region);
+  window.Ably.connection.on('connected', function() {
+    console.log("Connected to kode");
+  });
+  const channel = window.Ably.channels.get(`${appId}-${apiKey}`);
+  channel.publish("auth", `${appId}-connected`);
+}
